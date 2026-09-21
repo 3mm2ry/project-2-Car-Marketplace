4d84a97b3962b263baa7ef68a419704c68682373
Ammar Yaser <amm2rpriv@gmail.com>
2026-09-21 19:47:37 +0300

    edit the ERD

--- models/CarListing.js
+++ models/CarListing.js
@@ -1,6 +1,41 @@
 const mongoose = require("mongoose");
 
 const CarListingSchema = new mongoose.Schema({
+        streetAddress: {
+        type: String,
+        required: true
+    }
+    ,
+    brand: {
+    type: String,
+    required: true
+}
+,
+model: {
+    type: String,
+    required: true
+}
+,
+year: {
+    type: Number,
+    required: true
+}
+,
+price: {
+    type: Number,
+    required: true
+}
+,
+mileage: {
+    type: Number,
+    required: true
+}
+,
+description: {
+    type: String,
+    required: true
+}
+,
 
 }, {timestamps: true});
 
