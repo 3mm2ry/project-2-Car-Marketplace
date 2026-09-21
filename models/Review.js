846c14c59641a8cb1ad4431d829fff52da0e0c4a
Ammar Yaser <amm2rpriv@gmail.com>
2026-09-21 20:01:57 +0300

    Build Review model

--- models/Review.js
+++ models/Review.js
@@ -1,7 +1,30 @@
 const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
-
+    reviewBody: {
+    type: String,
+    required: true
+}
+,
+rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
+}
+,
+creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+}
+,
+carListing: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "CarListing",
+    required: true
+}
+,
 }, {timestamps: true});
 
 const Review = mongoose.model("Review", ReviewSchema);
